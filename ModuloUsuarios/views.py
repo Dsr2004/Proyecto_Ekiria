af600663a499dab18d1a9fcf9239713fd3312f80
Hoodinii890 <jhormangallegogallego@gmail.com>
2021-12-12 15:17:16 -0500

    nada

---
+++ ModuloUsuarios/views.py
@@ -0,0 +1,8 @@
+#-----------------------------------------Importaciones---------------------------------------------------
+from django.http import HttpResponse
+from django.template import Template, Context, loader 
+from django.shortcuts import render
+
+#--------------------------------------Cargadores de templates------------------------------------
+def login(request):
+    return render(request, "Information/login.html")
