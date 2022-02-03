69745aef2cb0285b42408a130fb7e3ae347c569e
Mariafmm <mariasillita28@gmail.com>
2022-02-03 10:41:50 -0500

    nada

--- Proyecto_Ekiria/settings.py
+++ Proyecto_Ekiria/settings.py
@@ -75,16 +75,25 @@ WSGI_APPLICATION = 'Proyecto_Ekiria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# esta es para mysql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'db_ekiria',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
+# para sqlite
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_ekiria',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
