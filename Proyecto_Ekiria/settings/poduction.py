eb450c78ef297268c18d8091b80d51ef68f3d9b1
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-03-10 17:07:44 -0500

    Comienzo sistema REST

---
+++ Proyecto_Ekiria/settings/poduction.py
@@ -0,0 +1,36 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# esta es para mysql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_ekiria',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+            'charset': 'utf8mb4',
+            }
+    }
+}
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+LOGIN_REDIRECT_URL = 'Inicio'
+LOGOUT_REDIRECT_URL = '/'
+
--- static/ModuloUsuarios/js/mainAdministration.js
+++ static/ModuloUsuarios/js/mainAdministration.js
@@ -10,42 +10,42 @@ const getValueInput = () => {
     document.getElementById("valueInput").innerHTML = inputValue;
 }
 
-function CambiarEstadoUsuario(id){
-    let ids=id
+function CambiarEstadoUsuario(id) {
+    let ids = id
     let token = $("#EstadoUsuarioForm").find('input[name=csrfmiddlewaretoken]').val()
     console.log(token)
     swal({
         title: "Estas seguro?",
-        text: "Se modificara el estado de el Tipo de Servicio",
+        text: "Se modificara el estado de el Usuario",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!'
-      }).then((willDelete) => {
+    }).then((willDelete) => {
         if (willDelete) {
-          swal("OK! Se ha modificado el tipo de servicio", {
-            icon: "success",
-          }).then(function() {
-              $.ajax({
-                data: {"csrfmiddlewaretoken":token, "estado":ids},
-                url: $("#EstadoUsuarioForm").attr('action'),
-                type: $("#EstadoUsuarioForm").attr('metho  d'),
-                success: function(data){
-                  window.location.href="/InformacionUsuario/Administracion/"
-                },
-                error: function(error){
-                  console.log("no")
-                  alert("Error:"+error.responseJSON)
-                }
-              }); 
-           
-       });
+            swal("OK! Se ha modificado el Usuario", {
+                icon: "success",
+            }).then(function() {
+                $.ajax({
+                    data: { "csrfmiddlewaretoken": token, "estado": ids },
+                    url: $("#EstadoUsuarioForm").attr('action'),
+                    type: $("#EstadoUsuarioForm").attr('method'),
+                    success: function(data) {
+                        window.location.href = "/InformacionUsuario/Administracion/"
+                    },
+                    error: function(error) {
+                        console.log("no")
+                        alert("Error:" + error.responseJSON)
+                    }
+                });
+
+            });
         } else {
-          swal("OK! Ningun dato del tipo de servicio ha sido modificado");
-          window.location.href="/InformacionUsuario/Administracion/"
+            swal("OK! Ningun dato del tipo de servicio ha sido modificado");
+            window.location.href = "/InformacionUsuario/Administracion/"
         }
-      });
-      return false;
-    
-    }
+    });
+    return false;
+
+}
