06b44ca2d6bf03d1b23b49a8630404f77fa8b475
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-03-10 20:17:24 -0500

    Cambios de texto por DOM

--- static/Configuracion/js/Cambios.js
+++ static/Configuracion/js/Cambios.js
@@ -1,11 +1,29 @@
 function CambioLetra() {
-   $(document).ready(function(){
-    let TipoLetra = document.querySelector("#TipoLetra").value;
-    let body = document.querySelectorAll("#text")[0];
-    if (body.style.fontFamily = TipoLetra){
-        body.style.fontFamily = TipoLetra
-        body = body.style.fontFamily = TipoLetra 
-        console.log(body)
-    }
-   })
-}   
+    $(document).ready(function() {
+        let TipoLetra = document.querySelector("#TipoLetra").value;
+        let body = document.querySelectorAll("#text");
+        body.forEach(texto => {
+            if (texto.style.fontFamily = TipoLetra) {
+                texto.style.fontFamily = TipoLetra
+                texto = texto.style.fontFamily = TipoLetra
+                console.log(texto)
+            }
+        });
+    })
+}
+list.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+        listElement.className = "list activar"
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if (menu.clientHeight == "0") {
+            height = menu.scrollHeight;
+            listElement.classList.toggle('arrow');
+            let list = document.querySelectorAll(".list_show")
+            for (let i = 0; i < list.length; i++) {
+                list[i].style.height = 0;
+            }
+        }
+        menu.style.height = height + "px";
+    })
+})
