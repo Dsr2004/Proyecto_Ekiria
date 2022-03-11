9c0831d38e64534a12bf9b32305205e4f0abffc4
Mariafmm <mariasillita28@gmai.com>
2022-03-10 19:43:33 -0500

    posicion de id para cambios

--- static/Configuracion/js/Cambios.js
+++ static/Configuracion/js/Cambios.js
@@ -1,11 +1,11 @@
 function CambioLetra() {
    $(document).ready(function(){
-    let TipoLetra = document.getElementById("TipoLetra").value;
-    let body = document.getElementById("texto")
+    let TipoLetra = document.querySelector("#TipoLetra").value;
+    let body = document.querySelectorAll("#text")[0];
     if (body.style.fontFamily = TipoLetra){
         body.style.fontFamily = TipoLetra
         body = body.style.fontFamily = TipoLetra 
         console.log(body)
     }
    })
-}
+}   
