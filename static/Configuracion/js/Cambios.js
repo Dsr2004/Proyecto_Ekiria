5f7e0b5bb0315dad2b63de2016020fc6c5283ae9
Mariafmm <mariasillita28@gmai.com>
2022-03-10 21:01:17 -0500

    letra checha

--- static/Configuracion/js/Cambios.js
+++ static/Configuracion/js/Cambios.js
@@ -11,19 +11,3 @@ function CambioLetra() {
         });
     })
 }
-list.forEach(listElement => {
-    listElement.addEventListener('click', () => {
-        listElement.className = "list activar"
-        let height = 0;
-        let menu = listElement.nextElementSibling;
-        if (menu.clientHeight == "0") {
-            height = menu.scrollHeight;
-            listElement.classList.toggle('arrow');
-            let list = document.querySelectorAll(".list_show")
-            for (let i = 0; i < list.length; i++) {
-                list[i].style.height = 0;
-            }
-        }
-        menu.style.height = height + "px";
-    })
-})
