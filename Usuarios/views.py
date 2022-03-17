1b11e0dcd87901179bcdd182ba8de23640b09d20
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-03-16 20:40:56 -0500

    nada

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -1,4 +1,4 @@
-#-----------------------------------------Import's---------------------------------------------------
+#-----------------------------------------Imports---------------------------------------------------
 from ast import Return
 from asyncio import transports
 from email import header
@@ -7,70 +7,76 @@ from msilib.schema import SelfReg
 from multiprocessing import context
 from pyexpat import model
 from re import template
+#-----------------------------------------Django---------------------------------------------------
 from django.http import HttpResponseRedirect, request, HttpResponse, JsonResponse
 from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DetailView, View
 from django.urls import reverse_lazy
-from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect
-from Usuarios.authentication_mixins import Authentication
+from django.contrib.sessions.models import Session
+from django.views.decorators.csrf import csrf_exempt
+#-----------------------------------------Rest Framework---------------------------------------------------
+from rest_framework.decorators import api_view
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from Usuarios.Serializers.general_serializers import UsuarioTokenSerializer
-from django.contrib.sessions.models import Session
-from datetime import datetime
 from rest_framework.views import APIView
+from rest_framework.renderers import TemplateHTMLRenderer
+from restt_framework import viewsets
+#-----------------------------------------Serializers---------------------------------------------------
+from Usuarios.Serializers.general_serializers import UsuarioTokenSerializer
+from Usuarios.Serializers.general_serializers import UsuarioTokenSerializer
+#-----------------------------------------Models---------------------------------------------------
 from Usuarios.models import Usuario
 from Ventas.models import Servicio
+#-----------------------------------------More---------------------------------------------------
+from Usuarios.authentication_mixins import Authentication
+from datetime import datetime
 from Usuarios.forms import Regitro, Editar
-from Usuarios.Serializers.general_serializers import UsuarioTokenSerializer
-from rest_framework.renderers import TemplateHTMLRenderer
-from django.views.decorators.csrf import csrf_exempt
-
 
 #--------------------------------------Templates Loaders------------------------------------
 class Login(ObtainAuthToken, TemplateView):
     template_name = 'registration/login.html'
     def post(self,request,*arg, **kwargs):
-        if request:
-            login_serializer = self.serializer_class(data = request.POST , context={'request':request})
-            print(request)
-            if login_serializer.is_valid():
-                user=login_serializer.validated_data['user']
-                if user.is_active:
-                    token,created = Token.objects.get_or_create(user=user)
-                    user_serializer = UsuarioTokenSerializer(user)
-                    Client = "http://127.0.0.1:8000/"
-                    if created:
-                        token = Token.objects.create(user = user)
-                        # header = {'Authorization':'Token '+token.key}
-                        # return Response(headers=header)
-                        return HttpResponseRedirect("/")
-                    else:
-                        all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
-                        if all_sessions.exists():
-                            for session in all_sessions:
-                                session_data = session.get_decoded()
-                                if user.id_usuario == int(session_data.get('_auth_user_id')):
-                                    session.delete()
-                        token.delete()
-                        token = Token.objects.create(user = user)
-                        # credentials = 'http://127.0.0.1:8000'
-                        # transport = HTTPTransport(credentials=credentials)
-                        # client = Client(transports=transport)
-                        # print(transport)
-                        # return Response(client)
-                        header = {'Authorization':'Token '+token.key}
-                        return Response(headers=header)
-                        # header = {'Authorization':'Token '+token.key}
-                        # return Response(headers=header, template_name="index.html")
+        pass
+        # if request:
+        #     login_serializer = self.serializer_class(data = request.POST , context={'request':request})
+        #     print(request)
+        #     if login_serializer.is_valid():
+        #         user=login_serializer.validated_data['user']
+        #         if user.is_active:
+        #             token,created = Token.objects.get_or_create(user=user)
+        #             user_serializer = UsuarioTokenSerializer(user)
+        #             Client = "http://127.0.0.1:8000/"
+        #             if created:
+        #                 token = Token.objects.create(user = user)
+        #                 # header = {'Authorization':'Token '+token.key}
+        #                 # return Response(headers=header)
+        #                 return HttpResponseRedirect("/")
+        #             else:
+        #                 all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
+        #                 if all_sessions.exists():
+        #                     for session in all_sessions:
+        #                         session_data = session.get_decoded()
+        #                         if user.id_usuario == int(session_data.get('_auth_user_id')):
+        #                             session.delete()
+        #                 token.delete()
+        #                 token = Token.objects.create(user = user)
+        #                 # credentials = 'http://127.0.0.1:8000'
+        #                 # transport = HTTPTransport(credentials=credentials)
+        #                 # client = Client(transports=transport)
+        #                 # print(transport)
+        #                 # return Response(client)
+        #                 header = {'Authorization':'Token '+token.key}
+        #                 return Response(headers=header)
+        #                 # header = {'Authorization':'Token '+token.key}
+        #                 # return Response(headers=header, template_name="index.html")
                     
                     
-                else:
-                    return Response({'error':'Este usuario no puede iniciar sesión'}, status = status.HTTP_401_UNAUTHORIZED)
-            else:
-                return Response({'error':'Contraseña o Usuario incorrectos'},status=status.HTTP_400_BAD_REQUEST)
+        #         else:
+        #             return Response({'error':'Este usuario no puede iniciar sesión'}, status = status.HTTP_401_UNAUTHORIZED)
+        #     else:
+        #         return Response({'error':'Contraseña o Usuario incorrectos'},status=status.HTTP_400_BAD_REQUEST)
 
 
                 
